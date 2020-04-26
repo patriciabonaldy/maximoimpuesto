7015c2bf5e2e4430707b306ec5b4e2725cc17027
patriciabonaldy <bonaldypatricia@gmail.com>
2020-04-26 20:07:33 -0300

    git inicial

---
+++ src/main/java/org/apache/hadoop/examples/MaxImpuestoByDireccionMapper.java
@@ -0,0 +1,25 @@
+package org.apache.hadoop.examples;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class MaxImpuestoByDireccionMapper extends Mapper<LongWritable, Text, Text, FloatWritable> {
+
+	@Override
+	public void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+
+		String line = value.toString();
+		String[] items = line.split(",");
+		
+		String stock = items[4];
+		Float closePrice = Float.parseFloat(items[20]);
+		
+		context.write(new Text(stock), new FloatWritable(closePrice));
+		
+	}
+}
